144aa1f5db38cb2cf48b113acc5dc7dee0eb3c7c
James Westwood <james.westwood@ons.gov.uk>
2021-02-01 18:17:14 +0000

    trying a new alter_data func to strip whitespace

--- scripts/build_data.py
+++ scripts/build_data.py
@@ -1,3 +1,11 @@
 from sdg.open_sdg import open_sdg_build
 
-open_sdg_build(config='config_data.yml')
+def alter_data(df):
+    # if 'Source details' in df:
+    #   del df['Source details']
+    # if 'concept.COMMENT_TS' in df:
+    #     del df['concept.COMMENT_TS']
+    df = data_frame.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+    return df
+
+open_sdg_build(config='config_data.yml', alter_data=alter_data)
--- scripts/check_data.py
+++ scripts/check_data.py
@@ -1,5 +1,13 @@
 from sdg.open_sdg import open_sdg_check
 
+def alter_data(df):
+    # if 'Source details' in df:
+    #   del df['Source details']
+    # if 'concept.COMMENT_TS' in df:
+    #     del df['concept.COMMENT_TS']
+    df = data_frame.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+    return df
+
 # Validate the indicators.
 validation_successful = open_sdg_check(config='config_data.yml')
 
