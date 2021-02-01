a6d987b6077da7ca98623d1d78e9c60e12b2b5a0
James Westwood <james.westwood@ons.gov.uk>
2021-02-01 18:44:51 +0000

    the whitespace is not being stripped. So testing if col exists

--- scripts/build_data.py
+++ scripts/build_data.py
@@ -3,6 +3,8 @@ from sdg.open_sdg import open_sdg_build
 def alter_data(df):
     df['COMMENT_OBS'] = df['COMMENT_OBS'].str.strip()
     df['SOURCE_DETAIL'] = df['SOURCE_DETAIL'].str.strip()
+    if not df['COMMENT_OBS']:
+        print("df['COMMENT_OBS'] does not exist")
     return df
 
 open_sdg_build(config='config_data.yml', alter_data=alter_data)
--- scripts/check_data.py
+++ scripts/check_data.py
@@ -3,6 +3,8 @@ from sdg.open_sdg import open_sdg_check
 def alter_data(df):
     df['COMMENT_OBS'] = df['COMMENT_OBS'].str.strip()
     df['SOURCE_DETAIL'] = df['SOURCE_DETAIL'].str.strip()
+    if not df['COMMENT_OBS']:
+        print("df['COMMENT_OBS'] does not exist")
     return df
 
 # Validate the indicators.
