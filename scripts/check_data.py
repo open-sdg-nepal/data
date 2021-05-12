8c871656a232f2866a18dee76f920972d085256d
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2021-05-12 19:40:14 +0100

    Update check_data.py

--- scripts/check_data.py
+++ scripts/check_data.py
@@ -1,12 +1,7 @@
 from sdg.open_sdg import open_sdg_check
 
-def alter_meta(meta, context):
-    if context['data'] is not None and context['data'].empty == False:
-        meta['reporting_status']='complete'
-    return meta
-
 # Validate the indicators.
-validation_successful = open_sdg_check(config='config_data.yml', alter_meta=alter_meta)
+validation_successful = open_sdg_check(config='config_data.yml')
 
 # If everything was valid, perform the build.
 if not validation_successful:
