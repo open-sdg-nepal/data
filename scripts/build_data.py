5d4aba8048c7fb51d26024e3bca375f335b8ed73
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2021-05-21 12:22:36 +0100

    Update build_data.py

--- scripts/build_data.py
+++ scripts/build_data.py
@@ -1,3 +1,9 @@
 from sdg.open_sdg import open_sdg_build
 
-open_sdg_build(config='config_data.yml')
+def alter_meta(meta, context):
+    print(context['data'])
+    if context['data'] is not None and context['data'].empty == False:
+        meta['reporting_status']='complete'
+    return meta
+
+open_sdg_build(config='config_data.yml', alter_meta=alter_meta)
